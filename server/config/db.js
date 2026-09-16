17e91672f7e53a8f2ae01d9c4eb5fdb2dd625e54
venkaiahkuncham123-tech <venkaiahkuncham123@gmail.com>
2026-09-16 14:08:32 +0530

    Replace mysql with mysql2 in db.js
    Switched from 'mysql' to 'mysql2' for database connection and updated connection code accordingly.

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+/* const mysql = require('mysql');
 
 // Use environment variables to configure the database connection
 const db = mysql.createConnection({
@@ -6,7 +6,16 @@ const db = mysql.createConnection({
   user: process.env.DB_USER || 'root',       // MySQL user
   password: process.env.DB_PASSWORD || 'password', // MySQL password
   database: process.env.DB_NAME || 'test_db', // MySQL database name
+});*/
+
+const mysql = require('mysql2');
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || 'mysql',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'test_db',
 });
+
 
 db.connect((err) => {
   if (err) {
