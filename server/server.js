b46fcda0dddd9fc89e33804782b9922f1b4dccd5
venkaiahkuncham123-tech <venkaiahkuncham123@gmail.com>
2026-09-16 11:27:20 +0530

    Change server port from 3000 to 5000

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000; // Use an environment variable for the port
+const port = process.env.PORT || 5000; // Use an environment variable for the port
 
 // Middleware
 app.use(cors());
